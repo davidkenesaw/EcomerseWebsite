e6db5f674016e10a3a2d21802141fdc59c833241
davidkenesaw <davidkennesaw@gmail.com>
2023-02-22 22:35:52 -0500

    login register 2 step auth

---
+++ Server/ServerProcessing/LoginRegister.js
@@ -0,0 +1,92 @@
+const {dbConn} = require('../../Config/db.config');
+const bcrypt = require('bcrypt')
+
+
+const saltRounds = 10;
+
+function Login(req,res){
+    const user = req.body.UserName;
+    const Password = req.body.Password;//add use of password
+
+    //database query
+    dbConn.query("SELECT * FROM Users WHERE UserName = ?", [user], function (err, rows) {
+
+        //if an error occures
+        if (err) {
+            const error = "there was an issue with your username or password";
+            res.render('Login', { error });
+        }
+        else {//log user in the redirect to Codepage
+            if (rows.length == 1) {
+                bcrypt.compare(Password, rows[0].Password, function (err, result) {
+                    if (result == true) {//if logged in is successful
+                        req.session.UserName = rows[0].UserName;
+                        req.session.FirstName = rows[0].FirstName;
+                        req.session.LastName = rows[0].LastName;
+                        req.session.Email = rows[0].Email;
+                        //add user info to users session
+                        req.session.loggedIn = true;
+                        req.session.Admin = rows[0].Admin;
+                        res.redirect('/TwoStepAuthPage');
+                    } else {
+                        const error = "UserName or Password is wrong";
+                        res.render('Login', { error });//this is wrong
+                    }
+                });
+
+            } else {//could not find user or password wrong
+                const error = "issue with username";
+                res.render('Login', { error });//this is wrong
+            }
+        }
+
+    });
+}
+function SignUp(req,res){
+    const UserName = req.body.UserName;
+    const Password = req.body.Password;
+
+    const FirstName = req.body.FirstName;
+    const LastName = req.body.LastName;
+    const Email = req.body.Email;
+
+    //encrypt 
+    bcrypt.hash(Password, saltRounds, function (err, hash) {
+        //database query
+        dbConn.query("INSERT INTO Users (UserName,Password,FirstName,LastName,Email) VALUES (?,?,?,?,?)", [UserName, hash, FirstName, LastName, Email], function (err, result) {
+
+            //if an error occures
+            if (err) {
+                console.log(err)
+                const error = "User Taken";
+                res.render('SignUp', { error });
+            } else {//register user
+                const error = "Signed in"
+                console.log("Data inserted");
+                res.render("SignUp",{error});//change back to redirect
+            }
+        });
+    });
+}
+function Authenticate(req,res){
+    const code = req.body.Code
+    console.log(code + " " + req.session.Code)
+    if(code == req.session.Code){
+        res.redirect("/HomeLoggedInPage")
+    }else{
+        const error = "Code was wrong"
+        res.render("TwoStepAuth",{error})
+    }
+}
+function checkLoggedIn(req, res,next){
+    
+    if(req.session.loggedIn){
+        return res.redirect("/HomeLoggedInPage")
+    }next()
+}
+function checknotLoggedIn(req, res,next){
+    if(!req.session.loggedIn){
+        return res.redirect("/")
+    }next()
+}
+module.exports = {SignUp, Login, Authenticate, checkLoggedIn, checknotLoggedIn};
--- Server/app.js
+++ Server/app.js
@@ -7,6 +7,8 @@ const express = require("express");
 
 const cookieParser = require('cookie-parser');
 const { seshOption } = require('../Config/db.config')
+const {SignUp, Login, Authenticate, checknotLoggedIn, checkLoggedIn} = require('./ServerProcessing/LoginRegister')
+const {sendEmail} = require('./Email/email')
 
 //configre express app
 const app = express();
@@ -20,9 +22,51 @@ app.set('views', path.join(__dirname, '../Client/views'));//show express the vie
 app.use(express.static(path.join(__dirname, '../Client')));//show express the Client directory
 app.use(seshOption)//configuration for express session
 
+//get requests 
+app.get('/', checkLoggedIn,function(req,res){
+    res.render("Home");
+});
+app.get('/HomeLoggedInPage', function(req,res){
+    const name = req.session.FirstName
+    res.render("HomeLoggedIn",{name});
+});
+app.get('/StorePage', function(req,res){
+    res.render("Store");
+});
+app.get('/CartPage', function(req,res){
+    res.render("Cart");
+});
+app.get('/LoginPage',checkLoggedIn, function(req,res){
+    const error = ""
+    res.render("Login",{error});
+});
+app.get('/SignUpPage', function(req,res){
+    const error = ""
+    res.render("SignUp",{error});
+});
+app.get('/TwoStepAuthPage', function(req,res){
+    const code = Math.floor(Math.random() * 1000) + 999;
+    req.session.Code = code;
+    sendEmail(req.session.Email, code)
+    
+    const error = ""
+    res.render("TwoStepAuth",{error});
+});
+app.get("/SignOut",checknotLoggedIn,function(req,res){
+    req.session.UserName = null;
+    req.session.FirstName = null;
+    req.session.Lastname = null;
+    req.session.Email = null;
+    req.session.loggedIn = null;
+    req.session.Admin = null;
+    req.session.Code = null;
+    res.render("Home")
+})
 
-
-
+//post requests
+app.post("/SignUp",SignUp);
+app.post("/Login",Login);
+app.post("/Authenticate",Authenticate);
 
 app.listen(process.env.PORT || 3456, function () {//host site
     console.log("Port: 3456");
